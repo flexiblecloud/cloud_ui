d965d5007edb1f81bb0aa9b26ff13e434d8751cf
Andrew <andriyvasyltsiv@gmail.com>
2019-09-02 16:06:02 +0300

    ...

--- setup.py
+++ setup.py
@@ -9,14 +9,26 @@ with open("README.md", "r") as fh:
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-setup(name='cloudui',
+
+def process_requirement(requirement):
+    if requirement.startswith("git+"):
+        dep_name = requirement.rpartition("@")[0].partition("/")[2]
+        return f"{dep_name}@{requirement}"
+    return requirement
+
+
+requirements = [process_requirement(requirement) for requirement in requirements]
+
+
+setup(
+    name='cloudui',
     version='0.0.1',
     description='Python trio and remi based library for making web-apps ...',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/flexiblecloud/cloud_ui',
     download_url='https://github.com/flexiblecloud/cloud_ui/archive/master.zip',
-    keywords=['gui-library','remi','trio','service','application','platform-independent','ui','gui'],
+    keywords=['gui-library', 'remi', 'trio', 'service', 'application', 'platform-independent', 'ui', 'gui'],
     author='Andriy Vasyltsiv',
     author_email='andriyvasyltsiv@gmail.com',
     license='BSD',
